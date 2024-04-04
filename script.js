303cb1400ab958e42ebba1bf500af8f6a987e298
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-04-04 17:36:55 +0700

    the javascript code

---
+++ script.js
@@ -0,0 +1,9 @@
+video.addEventListener("click", function video(){
+    alert("ini adalah video berisi orang sad boy")
+});
+audio.addEventListener("click", function audio(){
+    alert("ini adalah audio  cover lagu The Lazy Song milik Bruno Mars yang dibawakan oleh Gigi Vibes")
+});
+gambar.addEventListener("click", function gambar(){
+    alert("ini adalah gambar responsive ketika layar penuh dan tidak penuh akan mengubah gambar")
+})
